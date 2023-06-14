25559407c8828af644e853f996b6f0bef259d5c9
HazeL <105066838+HazeLdevgggg@users.noreply.github.com>
2023-06-14 15:58:55 +0200

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,7 +1,8 @@
 <?
-    $serveur = "sportmarludev.mysql.db"; // server name for php my admin
-    $login = "sportmarludev";  // login name for php my admin
-    $pass = "DevMadein34"; // password name for php my admin
-    $connection = new PDO("mysql:host=$serveur;dbname=sportmarludev",$login, $pass); // PDO connection
+    $serveur = "//"; // server name for php my admin
+    $login = "//";  // login name for php my admin
+    $pass = "//"; // password name for php my admin
+    $dbanme = "//"; // password name for php my admin
+    $connection = new PDO("mysql:host=$serveur;dbname=$dbname",$login, $pass); // PDO connection
     $connection -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);  // PDO Attribute
 ?>
