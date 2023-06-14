56a4883c260e99357498b83ff2ad07bbb0dce45a
HazeL <105066838+HazeLdevgggg@users.noreply.github.com>
2023-06-14 15:59:53 +0200

    Delete config.php

--- admin/config/config.php
+++
@@ -1,7 +0,0 @@
-<?
-    $serveur = "sportmarludev.mysql.db"; // server name for php my admin
-    $login = "sportmarludev";  // login name for php my admin
-    $pass = "DevMadein34"; // password name for php my admin
-    $connection = new PDO("mysql:host=$serveur;dbname=sportmarludev",$login, $pass); // PDO connection
-    $connection -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);  // PDO Attribute
-?>
