2a549f64193dee442f48c1771a7b1dda7a4198f3
HazeL <105066838+HazeLdevgggg@users.noreply.github.com>
2023-06-14 16:03:06 +0200

    Add files via upload

---
+++ admin/config/admin_table.php
@@ -0,0 +1,8 @@
+<?
+    $serveur = "//"; // server name for php my admin
+    $login = "//";  // login name for php my admin
+    $pass = "//"; // password name for php my admin
+    $dbname = "//"; // dbname name for php my admin
+    $connection = new PDO("mysql:host=$serveur;dbname=$dbname",$login, $pass); // PDO connection
+    $connection -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);  // PDO Attribute
+?>
